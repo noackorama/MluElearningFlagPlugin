a1041237123611d666e3764f2a071551435d70b0
noackorama <noackorama@gmail.com>
2018-05-30 18:05:49 +0200

    version 3

--- MluElearningFlagPlugin.php
+++ MluElearningFlagPlugin.php
@@ -20,6 +20,53 @@ class MluElearningFlagPlugin extends StudipPlugin implements SystemPlugin
      */
     function __construct()
     {
+        $this->descriptions = array();
+        $this->descriptions[1] = "Die Vorlesungsinhalte werden in Form von Videoaufzeichnungen zur Verfügung gestellt.";
+        $this->descriptions[2] = "Die Veranstaltung wird mit einer elektronischen Klausur (in ILIAS) abgeschlossen.";
+        $this->descriptions[3] = "Zu dieser Lehrveranstaltung gibt es ein unterstützendes Online­ bzw. Multimediales Lehr­Lernangebot (bspw. der Einsatz von TED­System, Selbsttest, Wiki, Forum, Online­Aufgaben etc.).";
+
+        if (basename($_SERVER['PHP_SELF']) == 'admin_seminare_assi.php'
+            && (Request::submitted('jump_back') && Request::get('form') == 6
+                || Request::submitted('jump_next') && Request::get('form') == 4)) {
+            $course = new Course();
+            $course->status = $_SESSION['sem_create_data']['sem_status'];
+            $df = current(DatafieldEntryModel::findByModel($course, $this->datafield_id));
+            if ($df) $df = $df->getTypedDatafield();
+            if ($df && $df->isEditable()) {
+                 $snippet = '<td width="90%" colspan="3">
+                <div style="text-align:left">
+                <h4>' . formatReady($df->getDescription()) . '</h4><ul style="list-style-type:none">';
+                $values = $_SESSION['sem_create_data']['sem_datafields'][$this->datafield_id]['value'] ? explode('|', $_SESSION['sem_create_data']['sem_datafields'][$this->datafield_id]['value']) : array();
+                foreach (current($df->getParams()) as $k => $v) {
+                    if (!$k) {
+                        $v = "Keine elektronischen bzw. Multimedia-Angebote";
+                    }
+                    $snippet .= '<li>';
+                    $snippet .= '<label>
+                    <input type="checkbox" name="sem_datafields['.$df->getId().'][]" value="'.$k.'" '.(in_array($k, $values) ? 'checked' : '').'>
+                    '.htmlReady($v).'</label>';
+                    if (isset($this->descriptions[$k])) {
+                        $snippet .= '&nbsp;' . tooltipIcon($this->descriptions[$k]);
+                    }
+                    if (!$k) $snippet .= '<hr>';
+                    $snippet .= '</li>';
+                }
+                $snippet .= '</ul></div></td>';
+
+                 $snippet = jsready($snippet, 'script-double');
+                 PageLayout::addHeadElement('script', array('type' => 'text/javascript'),"
+                     jQuery(function (\$) {
+                     \$('select[name^=\"sem_datafields[{$this->datafield_id}]\"]').closest('td').replaceWith('$snippet');
+                     \$('input[name^=\"sem_datafields[{$this->datafield_id}]\"]').
+                     first().on('change', function() {
+                        if (this.checked) {
+                            jQuery('input[name^=\"sem_datafields[{$this->datafield_id}]\"]:checked').attr('checked', false);
+                            this.checked = true;
+                        }
+                     });
+                     });");
+            }
+        }
         if (strpos($_SERVER['REQUEST_URI'], 'dispatch.php/course/basicdata') !== false) {
             $request_uri = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
             $course_id = array_pop(explode('/', $request_uri));
@@ -28,32 +75,40 @@ class MluElearningFlagPlugin extends StudipPlugin implements SystemPlugin
             if ($course && ($df = $course->datafields->findOneBy('datafield_id', $this->datafield_id))) {
                 $df = $df->getTypedDatafield();
             }
-            if ($course && $df->isEditable()) {
+            if ($df && $df->isEditable()) {
                 $snippet = '
                 <h2 id="bd_description2" class="table_row_even">'
-                . _("Angaben zu Elearning") .
+                . htmlReady($df->getName()) .
                 '</h2>
                 <div style="text-align:left">
                 <h3>' . formatReady($df->getDescription()) . '</h3><ul style="list-style-type:none">';
-                $values = $df->getValue() ? explode(',', $df->getValue()) : array();
+                $values = $df->getValue() ? explode('|', $df->getValue()) : array();
                 foreach (current($df->getParams()) as $k => $v) {
+                    if (!$k) {
+                        $v = "Keine elektronischen bzw. Multimedia-Angebote";
+                    }
                     $snippet .= '<li>';
                     $snippet .= '<label>
                     <input type="checkbox" name="datafields['.$df->getId().'][]" value="'.$k.'" '.(in_array($k, $values) ? 'checked' : '').'>
-                    '.htmlReady($v).'</label>
-                    </li>';
+                    '.htmlReady($v).'</label>';
+                    if (isset($this->descriptions[$k])) {
+                        $snippet .= '&nbsp;' . tooltipIcon($this->descriptions[$k]);
+                    }
+                    if (!$k) $snippet .= '<hr>';
+                    $snippet .= '</li>';
                 }
                 $snippet .= '</ul></div>';
 
                  $snippet = jsready($snippet, 'script-double');
                  PageLayout::addHeadElement('script', array('type' => 'text/javascript'),"
                      jQuery(function (\$) {
-                     \$('select[name^=\"datafields[{$this->datafield_id}]\"]').closest('tr').hide();
+                     \$('select[name^=\"datafields[{$this->datafield_id}]\"]').attr('disabled', true).closest('tr').hide();
                      \$('#settings h2').last().next('div').after('$snippet');
                      \$('input[name^=\"datafields[{$this->datafield_id}]\"]').
                      first().on('change', function() {
                         if (this.checked) {
-                        jQuery('input[name^=\"datafields[{$this->datafield_id}]\"]:checked').attr('checked', false);
+                            jQuery('input[name^=\"datafields[{$this->datafield_id}]\"]:checked').attr('checked', false);
+                            this.checked = true;
                         }
                      });
                      var active_accordion = jQuery('#settings').accordion('option','active');
@@ -71,15 +126,19 @@ class MluElearningFlagPlugin extends StudipPlugin implements SystemPlugin
         if (strpos($_SERVER['REQUEST_URI'], 'dispatch.php/profile') !== false) {
             $user = User::findByUsername(Request::username('username', $GLOBALS['user']->username));
             $the_kings = array_map('trim', split("[,;\n]",Config::get()->MLU_ELEARNING_KING));
-            if (in_array($user->username, $the_kings)) {
-                $snippet = '<div>
-                            <img title="KönigIn des ELearning"
-                            src="'.URLHelper::getScriptUrl('plugins_packages/data-quest/MluElearningFlagPlugin/images/king_of_elearning.png').'">
-                            </div>';
-                $snippet = jsready($snippet, 'script-double');
-                PageLayout::addHeadElement('script', array('type' => 'text/javascript'),"
-                     jQuery(function (\$) {\$('#user_profile tr td h1').before('$snippet');})");
+            foreach($the_kings as $one) {
+                list($king,$year) = explode(':', $one);
+                    if ($user->username == $king) {
+                    $snippet = '<div>
+                                <img title="Gewinner/in des @ward ' . $year . ' - Preis f&uuml;r multimediales Lehren und Lernen"
+                                src="'.URLHelper::getScriptUrl('plugins_packages/data-quest/MluElearningFlagPlugin/images/king_of_elearning_'.$year.'.png').'">
+                                </div>';
+                    $snippet = jsready($snippet, 'script-double');
+                    PageLayout::addHeadElement('script', array('type' => 'text/javascript'),"
+                         jQuery(function (\$) {\$('#user_profile tr td h1').before('$snippet');})");
+                }
             }
+
         }
         // set up tab navigation
         if ($GLOBALS['perm']->have_perm('root')) {
--- migrations/01_init_plugin.php
+++ migrations/01_init_plugin.php
@@ -6,9 +6,8 @@ class InitPlugin extends DBMigration
         try {
         DBManager::get()->exec("ALTER TABLE `datafields` CHANGE `type` `type` ENUM('bool','textline','textarea','selectbox','date','time','email','phone','radio','combo','link','selectboxmultiple') NOT NULL DEFAULT 'textline'");
         } catch (PDOException $e){}
-        DBManager::get()->exec("INSERT IGNORE INTO `datafields` (`datafield_id`, `name`, `object_type`, `object_class`, `edit_perms`, `view_perms`, `priority`, `mkdate`, `chdate`, `type`, `typeparam`, `is_required`, `description`) VALUES
-('e450484d672cd10ed04d790e475b4c16', 'Elearning', 'sem', '1', 'dozent', 'user', 99, 1402497350, 1402497492, 'selectboxmultiple', '=>\n1 => tolles Elearning\n2 => großartiges Elearning\n3 => umwerfendes Elearning', 0, 'Bitte geben Sie hier die Art von Elearning an, die in der Veranstaltung zum Einsatz kommt.');
-");
-        DBManager::get()->exec("INSERT IGNORE INTO `config` (`config_id`, `parent_id`, `field`, `value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`, `description`, `comment`, `message_template`) VALUES (MD5('MLU_ELEARNING_KING'), '', 'MLU_ELEARNING_KING', '', '1', 'string', 'global', 'mlu', '0', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Nutzername(n) des/der MLU-ELearning-Königs/Könige/Königin/Königinnen', '', '')");
+        DBManager::get()->exec("REPLACE INTO `datafields` (`datafield_id`, `name`, `object_type`, `object_class`, `edit_perms`, `view_perms`, `priority`, `mkdate`, `chdate`, `type`, `typeparam`, `is_required`, `description`) VALUES
+('e450484d672cd10ed04d790e475b4c16', 'Einsatz elektronischer oder multimedialer Lehr­-Lernformen', 'sem', '1', 'dozent', 'user', 99, 1402497350, 1426090223, 'selectboxmultiple', '=>\r\n1=>Videoaufzeichnung der Vorlesung\r\n2=>E­-Klausur\r\n3=>Elektronisches bzw. Multimediales Lehr­-Lernangebot', 0, 'Welches elektronische bzw. Multimedia-­Angebot setzten Sie in dieser Lehrveranstaltung ein?')");
+        DBManager::get()->exec("REPLACE INTO `config` (`config_id`, `parent_id`, `field`, `value`, `is_default`, `type`, `range`, `section`, `position`, `mkdate`, `chdate`, `description`, `comment`, `message_template`) VALUES (MD5('MLU_ELEARNING_KING'), '', 'MLU_ELEARNING_KING', '', '1', 'string', 'global', 'mlu', '0', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 'Nutzername(n) des/der MLU-ELearning-Königs/Könige/Königin/Königinnen', '', '')");
     }
 }
